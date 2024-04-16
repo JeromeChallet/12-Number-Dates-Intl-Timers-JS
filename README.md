7cf0770d945dca47896ae2cfb1728ae50c6ea4f0
AzureAD\JeromeChallet <cjerome@premiumartsinc.com>
2024-04-16 10:18:40 +0900

    Math and Rounding

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Number.parseInt()\
 Number.parseFloat()\
 Number.NaN()\
 Number.isFinite()\
+Math.sqrt()\
+Math.max()\
+Math.min()\
+Math.PI \* Number.parseFloat('10px') \*\* 2\
+Math.trunc(Math.random() \* 6) + 1\
+const randomInt = (min, max) =>\
+ Math.floor(Math.random() \* (max - min) + 1) + min;\
+Math.round()\
+Math.ceil()\
+Math.floor()\
+Math.trunc()\
+(x).toFixed(decimalsTohave)\
--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -288,3 +288,51 @@ console.log(Number.isFinite(23 / 0)); // false
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
+
+///////////////////////MATH AND ROUNDING//////////////////////////
+
+//square root
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+// cubic root
+console.log(8 ** (1 / 3));
+// get max value
+console.log(Math.max(5, 18, 23, 11, 2)); //23
+console.log(Math.max(5, 18, '23', 11, 2)); //23
+console.log(Math.max(5, 18, '23px', 11, 2)); //nan
+console.log(Math.min(5, 18, 23, 11, 2)); //2
+
+// surface area of a circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// random function between 2 values
+console.log(Math.trunc(Math.random() * 6) + 1);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
+
+// rounding integers
+console.log(Math.trunc(23.3));
+
+//round
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+//round up
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+// round down
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.9)); // 23
+
+console.log(Math.trunc(23.3)); // 23
+
+// floor works better than trunc to round
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding Decimals
+// must put the number in ()
+console.log((2.7).toFixed(0)); // 3 as a string
+console.log((2.7).toFixed(3)); // 2.700 as a string
+console.log((2.345).toFixed(2)); // 2.35 as a string
+console.log(+(2.345).toFixed(2)); // 2.35 as a number
